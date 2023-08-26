7e1b52c29cd1cbcf56ba2bc29931d95a41d9611d
tiendung8a6 <tiendung8a6@gmail.com>
2023-08-26 10:37:44 +0700

    Pagination for category

--- categories.php
+++ categories.php
@@ -2,7 +2,7 @@
 
 <div class="fluid-container">
   <?php
-  // get category id
+  // get the cat id
   if (isset($_GET['id'])) {
     $id = $_GET['id'];
     $sql = "SELECT * FROM categories WHERE cat_id = :id";
@@ -22,6 +22,30 @@
   </nav> <!--End nav-->
 
   <?php
+  $post_per_page = 4;
+  $status = "Published";
+  $sql2 = "SELECT * FROM posts WHERE post_cat_id = :id AND post_status = :status";
+  $stmt2 = $pdo->prepare($sql2);
+  $stmt2->execute([
+    ':id' => $_GET['id'],
+    ':status' => $status
+  ]);
+  $post_count = $stmt2->rowCount();
+  if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+    if ($page == 1) {
+      $page_id = 0;
+    } else {
+      $page_id = ($post_per_page * $page) - $post_per_page;
+    }
+  } else {
+    $page_id = 0;
+    $page = 1;
+  }
+  $total_pager = ceil($post_count / $post_per_page);
+  ?>
+
+  <?php
   if (isset($error)) {
     echo "<div class='alert alert-danger'>Page not found!</div>";
     exit;
@@ -31,9 +55,10 @@
   <section id="main" class="mx-5">
     <h2 class="my-3">Category: <?php echo $cat_t; ?></h2>
     <?php
-    $sql1 = "SELECT * FROM posts WHERE post_cat_id = :post_cat_id ";
+    $status = 'Published';
+    $sql1 = "SELECT * FROM posts WHERE post_cat_id = :post_cat_id AND post_status = :status LIMIT $page_id, $post_per_page";
     $stmt1 = $pdo->prepare($sql1);
-    $stmt1->execute([':post_cat_id' => $id]);
+    $stmt1->execute([':post_cat_id' => $id, ':status' => $status]);
     while ($post = $stmt1->fetch(PDO::FETCH_ASSOC)) {
       $post_id = $post['post_id'];
       $post_title = $post['post_title'];
@@ -59,18 +84,48 @@
     <?php } ?>
   </section>
 
-  <!-- <ul class="pagination px-5">
-    <li class="page-item disabled">
-      <a class="page-link" href="#" tabindex="-1">Previous</a>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item active">
-      <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item">
-      <a class="page-link" href="#">Next</a>
-    </li>
-  </ul> -->
+  <?php
+  if ($post_count > $post_per_page) { ?>
+    <ul class="pagination px-5">
+      <?php
+      if (isset($_GET['page'])) {
+        $prev = $_GET['page'] - 1;
+      } else {
+        $prev = 0;
+      }
+      if ($prev + 1 <= 1) {
+        echo '<li class="page-item disabled"><a class="page-link" href="#" tabindex="-1">Previous</a></li>';
+      } else {
+        echo '<li class="page-item"><a class="page-link" href="categories.php?id=' . $_GET['id'] . '&page=' . $prev . '" tabindex="-1">Previous</a></li>';
+      }
+      ?>
+      <?php
+      if (isset($_GET['page'])) {
+        $active = $_GET['page'];
+      } else {
+        $active = 1;
+      }
+      for ($i = 1; $i <= $total_pager; $i++) {
+        if ($i == $active) {
+          echo '<li class="page-item active"><a class="page-link" href="categories.php?id=' . $_GET['id'] . '&page=' . $i . '">' . $i . '</a></li>';
+        } else {
+          echo '<li class="page-item"><a class="page-link" href="categories.php?id=' . $_GET['id'] . '&page=' . $i . '">' . $i . '</a></li>';
+        }
+      }
+      ?>
+      <?php
+      if (isset($_GET['page'])) {
+        $next = $_GET['page'] + 1;
+      } else {
+        $next = 2;
+      }
+      if ($next - 1 >= $total_pager) {
+        echo '<li class="page-item disabled"><a class="page-link" href="#">Next</a></li>';
+      } else {
+        echo '<li class="page-item"><a class="page-link" href="categories.php?id=' . $_GET['id'] . '&page=' . $next . '">Next</a></li>';
+      }
+      ?>
+    </ul>
+  <?php } ?>
 
   <?php require_once('./includes/footer.php'); ?>
