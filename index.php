16533bfd08ad000a91cb20e3657ef418e6fae29a
tiendung8a6 <tiendung8a6@gmail.com>
2023-08-26 10:21:45 +0700

    Pagination Home

--- index.php
+++ index.php
@@ -5,24 +5,49 @@
     <?php require_once('./includes/navigation.php'); ?>
   </nav> <!--End nav-->
 
+  <?php
+  $post_per_page = 4;
+  $status = "Published";
+  $sql = "SELECT * FROM posts WHERE post_status = :status";
+  $stmt = $pdo->prepare($sql);
+  $stmt->execute([
+    ':status' => $status
+  ]);
+  $post_count = $stmt->rowCount();
+  if (isset($_GET['page'])) {
+    $page = $_GET['page'];
+    if ($page == 1) {
+      $page_id = 0;
+    } else {
+      $page_id = ($post_per_page * $page) - $post_per_page;
+    }
+  } else {
+    $page_id = 0;
+    $page = 1;
+  }
+  $total_pager = ceil($post_count / $post_per_page);
+  ?>
+
   <section id="main" class="mx-5">
     <h2 class="my-3">All Posts</h2>
     <?php
-    $sql = "SELECT * FROM posts";
+    $status = 'Published';
+    $sql = "SELECT * FROM posts WHERE post_status = :status LIMIT $page_id, $post_per_page";
     $stmt = $pdo->prepare($sql);
-    $stmt->execute();
+    $stmt->execute([
+      ':status' => $status
+    ]);
     while ($post = $stmt->fetch(PDO::FETCH_ASSOC)) {
       $post_id = $post['post_id'];
       $post_title = $post['post_title'];
-      $post_des = substr($post['post_des'], 0, 300);
+      $post_des = substr($post['post_des'], 0, 250);
       $post_image = $post['post_image'];
       $post_date = $post['post_date'];
       $post_author = $post['post_author'];
       $post_cat_id = $post['post_cat_id'];
       $post_status = $post['post_status']; ?>
-
       <div class="row my-4 single-post">
-        <img class="col col-lg-4 col-md-12" src="./img/<?php echo $post_image; ?>" alt="Image-<?php echo $post_image; ?>">
+        <img class="col col-lg-4 col-md-12" src="./img/<?php echo $post_image; ?>" alt="Image">
         <div class="media-body col col-lg-8 col-md-12">
           <h5 class="mt-0"><a href="single.php?id=<?php echo $post_id; ?>"><?php echo $post_title; ?></a></h5>
           <span class="posted"><a href="http://localhost:8080/blog/categories.php?id=<?php echo $post_cat_id; ?>" class="category">
@@ -42,23 +67,51 @@
           <span><a href="single.php?id=<?php echo $post_id; ?>" class="d-block">See more &rarr;</a></span>
         </div>
       </div>
-    <?php }
-    ?>
-
+    <?php } ?>
   </section>
 
-  <ul class="pagination px-5">
-    <li class="page-item disabled">
-      <a class="page-link" href="#" tabindex="-1">Previous</a>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">1</a></li>
-    <li class="page-item active">
-      <a class="page-link" href="#">2 <span class="sr-only">(current)</span></a>
-    </li>
-    <li class="page-item"><a class="page-link" href="#">3</a></li>
-    <li class="page-item">
-      <a class="page-link" href="#">Next</a>
-    </li>
-  </ul>
+  <?php
+  if ($post_count > $post_per_page) { ?>
+    <ul class="pagination px-5">
+      <?php
+      if (isset($_GET['page'])) {
+        $prev = $_GET['page'] - 1;
+      } else {
+        $prev = 0;
+      }
+      if ($prev + 1 <= 1) {
+        echo '<li class="page-item disabled"><a class="page-link" href="#" tabindex="-1">Previous</a></li>';
+      } else {
+        echo '<li class="page-item"><a class="page-link" href="index.php?page=' . $prev . '" tabindex="-1">Previous</a></li>';
+      }
+      ?>
+      <?php
+      if (isset($_GET['page'])) {
+        $active = $_GET['page'];
+      } else {
+        $active = 1;
+      }
+      for ($i = 1; $i <= $total_pager; $i++) {
+        if ($i == $active) {
+          echo '<li class="page-item active"><a class="page-link" href="index.php?page=' . $i . '">' . $i . '</a></li>';
+        } else {
+          echo '<li class="page-item"><a class="page-link" href="index.php?page=' . $i . '">' . $i . '</a></li>';
+        }
+      }
+      ?>
+      <?php
+      if (isset($_GET['page'])) {
+        $next = $_GET['page'] + 1;
+      } else {
+        $next = 2;
+      }
+      if ($next - 1 >= $total_pager) {
+        echo '<li class="page-item disabled"><a class="page-link" href="#">Next</a></li>';
+      } else {
+        echo '<li class="page-item"><a class="page-link" href="index.php?page=' . $next . '">Next</a></li>';
+      }
+      ?>
+    </ul>
+  <?php } ?>
 
-  <?php require_once("./includes/footer.php"); ?>
+<?php require_once("./includes/footer.php"); ?>
