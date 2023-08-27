bafb1d9adde62e8935e17732eea299e0570a2637
tiendung8a6 <tiendung8a6@gmail.com>
2023-08-27 08:13:34 +0700

    Work with comments

--- single.php
+++ single.php
@@ -56,39 +56,68 @@
       }
 ?>
 
-
-
 <div class="comments">
-  <h2 class="comment-count">3 Comments</h2>
-  <div class="comment-box">
-    <img src="./img/js.png" style="width:88px;height:88px;border-radius:50%" alt="Author photo" class="comment-photo">
-    <div class="comment-content">
-      <span class="comment-author"><b>Md. A. Barik</b></span>
-      <span class="comment-date">14 Dec 2018, 12:00PM</span>
-      <p class="comment-text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae aliquid doloremque repellendus aliquam sint saepe molestiae. Voluptas animi ut maiores quos amet tempora officia! Laboriosam nemo laborum enim vel provident?
-      </p>
-    </div>
-  </div>
-  <div class="comment-box">
-    <img src="./img/pdo.jpg" style="max-width:88px;max-height:88px;border-radius:50%" alt="Author photo" />
-    <div class="comment-content">
-      <span class="comment-author"><b>John Doe</b></span>
-      <span class="comment-date">14 Dec 2018, 12:00PM</span>
-      <p class="comment-text">
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae aliquid doloremque repellendus aliquam sint saepe molestiae. Voluptas animi ut maiores quos amet tempora officia! Laboriosam nemo laborum enim vel provident?
-      </p>
-    </div>
-  </div>
+  <?php
+  $sql2 = "SELECT * FROM comments WHERE comment_post_id = :id";
+  $stmt2 = $pdo->prepare($sql2);
+  $stmt2->execute([
+    ':id' => $_GET['id']
+  ]);
+  $comment_count = $stmt2->rowCount();
+  if ($comment_count == 0) {
+    echo "No comments";
+  } else {
+    echo '<h2 class="comment-count">' . $comment_count . ' Comments</h2>';
+    while ($comment = $stmt2->fetch(PDO::FETCH_ASSOC)) {
+      $comment_author = $comment['comment_author'];
+      $comment_des = $comment['comment_des'];
+      $comment_date = $comment['comment_date']; ?>
+
+      <div class="comment-box">
+        <img src="./img/profile.jpg" style="width:88px;height:88px;border-radius:50%" alt="Author photo" class="comment-photo">
+        <div class="comment-content">
+          <span class="comment-author"><b><?php echo $comment_date; ?></b></span>
+          <span class="comment-date"><?php echo $comment_author; ?></span>
+          <p class="comment-text"><?php echo $comment_des; ?></p>
+        </div>
+      </div>
+  <?php
+    }
+  } ?>
+
   <h3 class="leave-comment">Leave a comment</h3>
+  <?php
+  if (isset($_POST['submit-comment'])) {
+    $name = trim($_POST['name']);
+    $comment = ($_POST['comment']);
+    $date = date('j Y F');
+    if (empty($name) || empty($comment)) {
+      echo "<div class='alert alert-danger'>Please fill the form!</div>";
+    } else {
+      $sql4 = "INSERT INTO comments (comment_des, comment_date, comment_author, comment_post_id) VALUES (:comment, :date, :author, :cp_id)";
+      $stmt4 = $pdo->prepare($sql4);
+      $stmt4->execute([
+        ':comment' => $comment,
+        ':date' => $date,
+        ':author' => $name,
+        ':cp_id' => $_GET['id']
+      ]);
+      $sql5 = "UPDATE posts SET post_comment = post_comment + 1 WHERE post_id = :id";
+      $stmt5 = $pdo->prepare($sql5);
+      $stmt5->execute([':id' => $id]);
+      header("Location: single.php?id={$id}");
+    }
+  }
+  ?>
+
   <div class="comment-submit">
-    <form action="#" class="comment-form">
-      <input class="input" type="text" placeholder="Enter Full Name">
-      <input class="input" type="email" placeholder="Enter valid email">
+    <form class="comment-form" method="POST" action="http://localhost:8080/blog/single.php?id=<?php echo $_GET['id']; ?>">
+      <input name="name" class="input" type="text" placeholder="Enter Full Name">
       <textarea name="comment" id="" cols="20" rows="5" placeholder="Comment text"></textarea>
-      <input type="submit" value="Submit" class="comment-btn">
+      <input type="submit" value="Submit" name="submit-comment" class="comment-btn">
     </form>
   </div>
+
 </div>
   </section>
 
